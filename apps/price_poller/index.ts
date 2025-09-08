98589e4ed20f27a303c6569890c5dd45523749ba
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2025-09-09 00:36:15 +0530

    bun ws connected to binance

--- apps/price_poller/index.ts
+++ apps/price_poller/index.ts
@@ -1,8 +1,22 @@
-const server = Bun.serve({
-  port: 3000,
-  fetch(req) {
-    return new Response("Price Poller");
-  },
+const symbol = "btcusdt";
+const interval = "1m";
+
+const BINANCE_STEAM_URL = `wss://stream.binance.com:9443/ws/${symbol}@kline_${interval}`;
+
+const ws = new WebSocket(BINANCE_STEAM_URL);
+
+ws.addEventListener("open", (event) => {
+  console.log("connected to binance stream");
 });
 
-console.log(`Listening on http://localhost:${server.port}...`);
+ws.addEventListener("message", (event) => {
+  console.log("message: ", JSON.stringify(event.data));
+});
+
+ws.addEventListener("close", (event) => {
+  console.log("clossing connection to binance");
+});
+
+ws.addEventListener("error", (event) => {
+  console.log("error in connection");
+});
